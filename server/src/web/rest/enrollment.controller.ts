06bd2eede4a1c364075a48269825d394df76d888
Alfonso Ballesteros <ponchob1258@gmail.com>
2021-03-29 19:12:20 -0500

    feat: fix enrollment

--- server/src/web/rest/enrollment.controller.ts
+++ server/src/web/rest/enrollment.controller.ts
@@ -11,9 +11,9 @@ import { UploadFileService } from '../../service/uploadfile.service';
 import { UpdateEnrollmentDto } from '../../service/dto/update-enrollment.dto';
 
 @Controller('api/enrollments')
-//@UseGuards(AuthGuard, RolesGuard)
+@UseGuards(AuthGuard, RolesGuard)
 @UseInterceptors(LoggingInterceptor)
-//@ApiBearerAuth()
+@ApiBearerAuth()
 @ApiUseTags('enrollments')
 export class EnrollmentController {
   logger = new Logger('EnrollmentController');
@@ -63,7 +63,7 @@ export class EnrollmentController {
   }
 
   @PostMethod('/')
-  //@Roles(RoleType.STUDENT, RoleType.ADMIN)
+  @Roles(RoleType.STUDENT, RoleType.ADMIN)
   @ApiOperation({ title: 'Create enrollment' })
   @ApiResponse({
     status: 201,
